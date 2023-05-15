fd4bc8ea77bd00d1e66c1d3e61a66d4f5ca0bcb7
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2023-05-15 04:19:44 +0000

    add pm2 config file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'ms2_handbook',
+            interpreter: 'node',
+            interpreterArgs: '-r dotenv/config build',
+            script: 'build/index.js',
+        },
+    ],
+};
