7e197ff300724d4d01f01ccd6bd74c9d29b3275e
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2023-07-24 22:04:37 -0300

    feat: add highlight.io

---
+++ src/hooks.client.ts
@@ -0,0 +1,11 @@
+import { H } from 'highlight.run';
+
+H.init('jd4100d5', {
+	environment: 'production',
+	version: '0.1.1',
+	tracingOrigins: true,
+	networkRecording: {
+		enabled: true,
+		recordHeadersAndBody: true
+	}
+});
