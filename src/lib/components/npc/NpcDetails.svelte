1b249500738fb2e9a78f3130e1511ce769856f24
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2024-07-15 13:26:02 -0300

    feat: npc stats

--- src/lib/components/npc/NpcDetails.svelte
+++ src/lib/components/npc/NpcDetails.svelte
@@ -1,61 +1,104 @@
 <script lang="ts">
   import Link from '$lib/components/Link.svelte';
-  import type { Npc } from '../../types/Npc';
+  import { toReadableStat, type Npc } from '../../types/Npc';
   import ItemListContainer from '../item/ItemListContainer.svelte';
   import NpcImage from './NpcImage.svelte';
 
   export let npc: Npc;
   // todo:
-  // - stats
   // - skills
+
+  let toggleStats: boolean = false;
 </script>
 
 <ItemListContainer>
+  <div class="mb-2 flex">
+    <NpcImage name={npc.name} portrait={npc.portrait} />
+  </div>
   <div>
-    <div class="mb-2">
-      <NpcImage name={npc.name} portrait={npc.portrait} />
+    <div class="flex flex-row gap-1">
+      <p>Stats:</p>
+      <button
+        class="text-ascendant cursor-pointer"
+        on:click={() => {
+          toggleStats = !toggleStats;
+        }}
+      >
+        [Click here to {toggleStats ? 'retract' : 'expand'}]
+      </button>
     </div>
-    <div class="flex flex-col">
-      {#if npc.npc_type === 0}
-        <div>
-          Level: {npc.level}
-        </div>
-      {/if}
-      {#if npc.class_name && npc.npc_type === 0}
-        <div>
-          Type: {npc.class_name}
-        </div>
-      {/if}
-      {#if npc.race}
-        <div>
-          Category: {npc.race}
-        </div>
-      {/if}
-      {#if npc.shop_id}
-        <p>
-          Shop ID: {npc.shop_id}
-        </p>
-      {/if}
-      {#if npc.field_metadata?.length > 0}
-        <hr class="my-4" />
-        <div class="flex flex-col">
-          {#if npc.npc_type === 0}
-            Main Habitat:
-          {:else}
-            Found in:
+    <div class="limit-size {toggleStats ? '' : 'limit-size--active'}">
+      <ul>
+        {#each Object.entries(npc.stats) as [key, value], index}
+          {#if value > 0}
+            <li>
+              {toReadableStat(key)}: {value}
+            </li>
           {/if}
-          <div>
-            {#each npc.field_metadata as field, index}
-              <Link href={`/maps/${field.Item2}`}>
-                {field.Item1}
-              </Link>
-              {#if index < npc.field_metadata.length - 1}
-                <span class="-ml-1">, </span>
-              {/if}
-            {/each}
-          </div>
-        </div>
-      {/if}
+        {/each}
+      </ul>
     </div>
   </div>
+  <div class="flex flex-col mt-2">
+    {#if npc.npc_type === 0}
+      <div>
+        Level: {npc.level}
+      </div>
+    {/if}
+    {#if npc.class_name && npc.npc_type === 0}
+      <div>
+        Type: {npc.class_name}
+      </div>
+    {/if}
+    {#if npc.race}
+      <div>
+        Category: {npc.race}
+      </div>
+    {/if}
+    {#if npc.shop_id}
+      <p>
+        Shop ID: {npc.shop_id}
+      </p>
+    {/if}
+    {#if npc.field_metadata?.length > 0}
+      <hr class="my-4" />
+      <div class="flex flex-col">
+        {#if npc.npc_type === 0}
+          Main Habitat:
+        {:else}
+          Found in:
+        {/if}
+        <div>
+          {#each npc.field_metadata as field, index}
+            <Link href={`/maps/${field.Item2}`}>
+              {field.Item1}
+            </Link>
+            {#if index < npc.field_metadata.length - 1}
+              <span class="-ml-1">, </span>
+            {/if}
+          {/each}
+        </div>
+      </div>
+    {/if}
+  </div>
 </ItemListContainer>
+
+<style lang="scss">
+  ul {
+    list-style-type: disc;
+    list-style-position: inside;
+  }
+
+  .text-ascendant {
+    color: #ff8c37;
+  }
+
+  .limit-size {
+    &--active {
+      display: -webkit-box;
+      -webkit-line-clamp: 4;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+    }
+  }
+</style>
--- src/lib/types/Npc.ts
+++ src/lib/types/Npc.ts
@@ -84,4 +84,129 @@ export interface Stats {
   ScaleBaseSpaRate4: number;
 }
 
+export function toReadableStat(stat: string): string {
+  switch (stat) {
+    case 'Str':
+      return 'Strength';
+    case 'Dex':
+      return 'Dexterity';
+    case 'Int':
+      return 'Intelligence';
+    case 'Luk':
+      return 'Luck';
+    case 'Hp':
+      return 'Health';
+    case 'HpRgp':
+      return 'Health Regen';
+    case 'HpInv':
+      return 'Health Regen Interval';
+    case 'Sp':
+      return 'Spirit';
+    case 'SpRgp':
+      return 'Spirit Regen';
+    case 'SpInv':
+      return 'Spirit Regen Interval';
+    case 'Ep':
+      return 'Stamina';
+    case 'EpRgp':
+      return 'Stamina Regen';
+    case 'EpInv':
+      return 'Stamina Regen Interval';
+    case 'Asp':
+      return 'Attack Speed';
+    case 'Msp':
+      return 'Movement Speed';
+    case 'Atp':
+      return 'Attack Power';
+    case 'Evp':
+      return 'Evasion';
+    case 'Cap':
+      return 'Critical Rate';
+    case 'Cad':
+      return 'Critical Damage';
+    case 'Car':
+      return 'Critical Evasion';
+    case 'Ndd':
+      return 'Normal Damage Decrease';
+    case 'Abp':
+      return 'Perfect Guard';
+    case 'Jmp':
+      return 'Jump';
+    case 'Pap':
+      return 'Physical Armor Penetration';
+    case 'Map':
+      return 'Magic Armor Penetration';
+    case 'Par':
+      return 'Physical Resistance';
+    case 'Mar':
+      return 'Magic Resistance';
+    case 'Wapmin':
+      return 'Weapon Minimum Damage';
+    case 'Wapmax':
+      return 'Weapon Maximum Damage';
+    case 'Dmg':
+      return 'Damage';
+    case 'Pen':
+      return 'Piercing';
+    case 'Rmsp':
+      return 'Mount Speed';
+    case 'Bap':
+      return 'Bonus Attack Power';
+    case 'BapPet':
+      return 'Bonus Attack Power (Pet)';
+    case 'Hiddenhpadd':
+      return 'Hidden Health Add';
+    case 'Hiddennddadd':
+      return 'Hidden Normal Damage Decrease Add';
+    case 'Hiddenwapadd':
+      return 'Hidden Weapon Damage Add';
+    case 'Hiddenhpadd03':
+      return 'Hidden Health Add 03';
+    case 'Hiddennddadd03':
+      return 'Hidden Normal Damage Decrease Add 03';
+    case 'Hiddenwapadd03':
+      return 'Hidden Weapon Damage Add 03';
+    case 'Hiddenhpadd04':
+      return 'Hidden Health Add 04';
+    case 'Hiddennddadd04':
+      return 'Hidden Normal Damage Decrease Add 04';
+    case 'Hiddenwapadd04':
+      return 'Hidden Weapon Damage Add 04';
+    case 'ScaleStatRate1':
+      return 'Scale Stat Rate 1';
+    case 'ScaleStatRate2':
+      return 'Scale Stat Rate 2';
+    case 'ScaleStatRate3':
+      return 'Scale Stat Rate 3';
+    case 'ScaleStatRate4':
+      return 'Scale Stat Rate 4';
+    case 'ScaleBaseTap1':
+      return 'Scale Base Attack Power 1';
+    case 'ScaleBaseTap2':
+      return 'Scale Base Attack Power 2';
+    case 'ScaleBaseTap3':
+      return 'Scale Base Attack Power 3';
+    case 'ScaleBaseTap4':
+      return 'Scale Base Attack Power 4';
+    case 'ScaleBaseDef1':
+      return 'Scale Base Defense 1';
+    case 'ScaleBaseDef2':
+      return 'Scale Base Defense 2';
+    case 'ScaleBaseDef3':
+      return 'Scale Base Defense 3';
+    case 'ScaleBaseDef4':
+      return 'Scale Base Defense 4';
+    case 'ScaleBaseSpaRate1':
+      return 'Scale Base Special Rate 1';
+    case 'ScaleBaseSpaRate2':
+      return 'Scale Base Special Rate 2';
+    case 'ScaleBaseSpaRate3':
+      return 'Scale Base Special Rate 3';
+    case 'ScaleBaseSpaRate4':
+      return 'Scale Base Special Rate 4';
+    default:
+      return stat;
+  }
+}
+
 export type SearchNpc = Pick<Npc, 'id' | 'name' | 'portrait' | 'title'>;
